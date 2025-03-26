05e5ff7d037186e492c03ebde495bd0ecb329198
A2ZTechy <95485382+A2ZTechy@users.noreply.github.com>
2025-03-26 22:21:51 +0530

    server.js

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(3000, () => {
-    console.log("Server running on http://localhost:8080");
+    console.log("Server running on http://localhost:3000");
 });
