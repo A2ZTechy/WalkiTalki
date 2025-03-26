aca628ef827023cff743286f3346231f3a25986f
A2ZTechy <95485382+A2ZTechy@users.noreply.github.com>
2025-03-26 22:38:14 +0530

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -9,6 +10,15 @@ const io = socketIo(server, {
     cors: { origin: "*" }
 });
 
+// Allow Express to serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve `index.html` when visiting the homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// WebSocket logic
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
@@ -26,6 +36,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+// Use the port provided by Render OR default to 3000 locally
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
